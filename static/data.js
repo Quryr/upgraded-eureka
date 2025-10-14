8c91f99f903d65cef1a2801ed92a786b6d57bead
Quryr <daturafox2000@gmail.com>
2025-10-14 10:24:45 +0300

    Update data.js

--- static/data.js
+++ static/data.js
@@ -1,33 +1,33 @@
 const casesData = {
   "luxury-style": [
-    { name: "Crystal Candy", img: "/static/assets/cases/crystal_candy.png", price: 550 },
-    { name: "Glam Lock", img: "/static/assets/cases/glam_lock.png", price: 700 },
-    { name: "Midnight Charm", img: "/static/assets/cases/midnight_charm.png", price: 850 },
-    { name: "Silver Soul", img: "/static/assets/cases/silver_soul.png", price: 1000 },
-    { name: "Golden Pulse", img: "/static/assets/cases/golden_pulse.png", price: 1150 }
+    { id: "crystal_candy", name: "Crystal Candy", img: "/static/assets/cases/crystal_candy.png", price: 550 },
+    { id: "glam_lock", name: "Glam Lock", img: "/static/assets/cases/glam_lock.png", price: 700 },
+    { id: "midnight_charm", name: "Midnight Charm", img: "/static/assets/cases/midnight_charm.png", price: 850 },
+    { id: "silver_soul", name: "Silver Soul", img: "/static/assets/cases/silver_soul.png", price: 1000 },
+    { id: "golden_pulse", name: "Golden Pulse", img: "/static/assets/cases/golden_pulse.png", price: 1150 }
   ],
 
   "mystic-energy": [
-    { name: "Dark Rhythm", img: "/static/assets/cases/dark_rhythm.png", price: 600 },
-    { name: "Ghost Light", img: "/static/assets/cases/ghost_light.png", price: 750 },
-    { name: "Moon Sigil", img: "/static/assets/cases/moon_sigil.png", price: 850 },
-    { name: "Inferno Eye", img: "/static/assets/cases/inferno_eye.png", price: 1000 },
-    { name: "Energy Core", img: "/static/assets/cases/energy_core.png", price: 1150 }
+    { id: "dark_rhythm", name: "Dark Rhythm", img: "/static/assets/cases/dark_rhythm.png", price: 600 },
+    { id: "ghost_light", name: "Ghost Light", img: "/static/assets/cases/ghost_light.png", price: 750 },
+    { id: "moon_sigil", name: "Moon Sigil", img: "/static/assets/cases/moon_sigil.png", price: 850 },
+    { id: "inferno_eye", name: "Inferno Eye", img: "/static/assets/cases/inferno_eye.png", price: 1000 },
+    { id: "energy_core", name: "Energy Core", img: "/static/assets/cases/energy_core.png", price: 1150 }
   ],
 
   "memes-fun": [
-    { name: "Snoop Pack", img: "/static/assets/cases/snoop_pack.png", price: 400 },
-    { name: "Pepe Drop", img: "/static/assets/cases/pepe_drop.png", price: 500 },
-    { name: "Cool Doggo", img: "/static/assets/cases/cool_doggo.png", price: 650 },
-    { name: "Party Crew", img: "/static/assets/cases/party_crew.png", price: 800 },
-    { name: "Ban Hammer", img: "/static/assets/cases/ban_hammer.png", price: 950 }
+    { id: "snoop_pack", name: "Snoop Pack", img: "/static/assets/cases/snoop_pack.png", price: 400 },
+    { id: "pepe_drop", name: "Pepe Drop", img: "/static/assets/cases/pepe_drop.png", price: 500 },
+    { id: "cool_doggo", name: "Cool Doggo", img: "/static/assets/cases/cool_doggo.png", price: 650 },
+    { id: "party_crew", name: "Party Crew", img: "/static/assets/cases/party_crew.png", price: 800 },
+    { id: "ban_hammer", name: "Ban Hammer", img: "/static/assets/cases/ban_hammer.png", price: 950 }
   ],
 
   "collectibles": [
-    { name: "B-Day Box", img: "/static/assets/cases/bday_box.png", price: 800 },
-    { name: "Retro Wave", img: "/static/assets/cases/retro_wave.png", price: 950 },
-    { name: "Infinity Vault", img: "/static/assets/cases/infinity_vault.png", price: 1100 },
-    { name: "Toxic Vibes", img: "/static/assets/cases/toxic_vibes.png", price: 1250 },
-    { name: "Emerald Blaze", img: "/static/assets/cases/emerald_blaze.png", price: 1400 }
+    { id: "bday_box", name: "B-Day Box", img: "/static/assets/cases/bday_box.png", price: 800 },
+    { id: "retro_wave", name: "Retro Wave", img: "/static/assets/cases/retro_wave.png", price: 950 },
+    { id: "infinity_vault", name: "Infinity Vault", img: "/static/assets/cases/infinity_vault.png", price: 1100 },
+    { id: "toxic_vibes", name: "Toxic Vibes", img: "/static/assets/cases/toxic_vibes.png", price: 1250 },
+    { id: "emerald_blaze", name: "Emerald Blaze", img: "/static/assets/cases/emerald_blaze.png", price: 1400 }
   ]
 };
