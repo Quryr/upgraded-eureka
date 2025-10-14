7135496f6afcceb573c9f1a51142da4db4b0e1e7
Quryr <daturafox2000@gmail.com>
2025-10-14 10:51:35 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static', template_folder='.')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-# Главная страница
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# Страница кейса
 @app.route('/case.html')
-def case_page():
+def case():
     return render_template('case.html')
 
-# Обслуживание статических файлов
-@app.route('/static/<path:filename>')
-def static_files(filename):
-    return app.send_static_file(filename)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
