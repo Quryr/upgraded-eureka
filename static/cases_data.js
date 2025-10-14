50df4f1684ecf41004fad62cbe6bca6711b18e72
Quryr <daturafox2000@gmail.com>
2025-10-14 11:43:33 +0300

    Create cases_data.js

---
+++ static/cases_data.js
@@ -0,0 +1,23 @@
+// === Все кейсы ===
+const allCases = [
+  { id: "crystal_candy", name: "Crystal Candy", img: "/static/assets/cases/crystal_candy.png", price: 550 },
+  { id: "glam_lock", name: "Glam Lock", img: "/static/assets/cases/glam_lock.png", price: 700 },
+  { id: "midnight_charm", name: "Midnight Charm", img: "/static/assets/cases/midnight_charm.png", price: 850 },
+  { id: "silver_soul", name: "Silver Soul", img: "/static/assets/cases/silver_soul.png", price: 1000 },
+  { id: "golden_pulse", name: "Golden Pulse", img: "/static/assets/cases/golden_pulse.png", price: 1150 },
+
+  { id: "dark_rhythm", name: "Dark Rhythm", img: "/static/assets/cases/dark_rhythm.png", price: 600 },
+  { id: "ghost_light", name: "Ghost Light", img: "/static/assets/cases/ghost_light.png", price: 750 },
+  { id: "moon_sigil", name: "Moon Sigil", img: "/static/assets/cases/moon_sigil.png", price: 850 },
+  { id: "inferno_eye", name: "Inferno Eye", img: "/static/assets/cases/inferno_eye.png", price: 1000 },
+  { id: "energy_core", name: "Energy Core", img: "/static/assets/cases/energy_core.png", price: 1150 },
+
+  { id: "snoop_pack", name: "Snoop Pack", img: "/static/assets/cases/snoop_pack.png", price: 400 },
+  { id: "pepe_drop", name: "Pepe Drop", img: "/static/assets/cases/pepe_drop.png", price: 500 },
+  { id: "cool_doggo", name: "Cool Doggo", img: "/static/assets/cases/cool_doggo.png", price: 650 },
+  { id: "party_crew", name: "Party Crew", img: "/static/assets/cases/party_crew.png", price: 800 },
+  { id: "ban_hammer", name: "Ban Hammer", img: "/static/assets/cases/ban_hammer.png", price: 950 }
+];
+
+// === Делаем доступным глобально ===
+window.allCases = allCases;
