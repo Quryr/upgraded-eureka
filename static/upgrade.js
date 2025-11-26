f3aa639631ff1185e5355faee299696c6fad1a65
Quryr <daturafox2000@gmail.com>
2025-11-26 12:24:39 +0300

    Update upgrade.js

--- static/upgrade.js
+++ static/upgrade.js
@@ -1,7 +1,28 @@
-console.log("Upgrade page loaded");
+document.addEventListener("DOMContentLoaded", () => {
+    const user = loadUser();
+    if (!user || !user.inventory) return;
 
-// позже добавим:
-// - выбор предметов
-// - расчёт шанса
-// - анимация крутилки
-// - результат (успех / провал)
+    const inv = document.getElementById("upgrade-inventory");
+    inv.innerHTML = "";
+
+    user.inventory.forEach((item, i) => {
+        const div = document.createElement("div");
+        div.className = "upgrade-item";
+
+        div.innerHTML = `
+            <img src="${item.img}" class="upgrade-item-img">
+            <p class="upgrade-item-name">${item.name}</p>
+            <p class="upgrade-item-price">${item.price} ⭐</p>
+        `;
+
+        div.onclick = () => {
+            document.getElementById("upgrade-source").innerHTML = `
+                <img src="${item.img}" class="upgrade-selected-img">
+                <p class="upgrade-selected-name">${item.name}</p>
+                <p class="upgrade-selected-price">${item.price} ⭐</p>
+            `;
+        };
+
+        inv.appendChild(div);
+    });
+});
